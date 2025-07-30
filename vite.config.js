c165e872cc0e3032224173fa090ab462b50f4ab1
ajalil01 <ahmed.benzid@etu.univ-batna2.dz>
2025-07-30 11:24:10 -0700

    deploying

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/puzzle_landing_page/'
+  base: '/'
 });
